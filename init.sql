b84279cf3a85d26d92d3a8ef3006a9ad2b31d7d2
Artem Zhuikov <mindtable@yandex-team.ru>
2023-01-06 21:31:45 +0300

    feat: add docker compose and initial sql script

---
+++ init.sql
@@ -0,0 +1,7 @@
+USE [master]
+RESTORE DATABASE [AdventureWorks2017]
+FROM DISK = '/var/opt/mssql/backup/AdventureWorks2017.bak'
+WITH MOVE 'AdventureWorks2017' TO '/var/opt/mssql/data/AdventureWorks2017.mdf',
+MOVE 'AdventureWorks2017_log' TO '/var/opt/mssql/data/AdventureWorks2017_log.ldf',
+FILE = 1,  NOUNLOAD,  STATS = 5
+GO
